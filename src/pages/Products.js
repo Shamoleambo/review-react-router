a7ffc401f82fb76d71aa05d4a66650c4a522c178
Tiago Gomes <tidgomes@gmail.com>
2023-08-16 12:51:37 -0300

    dynamically set the route using provided id

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,21 @@
+import { Link } from 'react-router-dom'
+
+const products = [
+  { id: 'p1', title: 'Product 1' },
+  { id: 'p2', title: 'Product 2' },
+  { id: 'p3', title: 'Product 3' }
+]
+
 const ProductsPage = () => {
   return (
     <>
       <p>Products page</p>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        {products.map((product) => (
+          <li key={product.id}>
+            <Link to={`/products/${product.id}`}>{product.title}</Link>
+          </li>
+        ))}
       </ul>
     </>
   )
